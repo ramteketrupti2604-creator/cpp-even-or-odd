33d392830088df8c42a3588f9ac7340a6c7b82e9
ramteketrupti2604-creator <ramteketrupti2604@gmail.com>
2026-01-13 14:02:02 +0530

    Add files via upload

---
+++ evenorodd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=22;
+    if(n/2){
+        cout<<"number is even";
+    }
+    else{
+        cout<<"number is odd";
+    }
+    return 0;
+}
